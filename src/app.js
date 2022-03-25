6e481ee2ad8db14c75a3ea5730d703aa2c4a4b4f
Utkarsh Saxena <utkarsh@pop-os.localdomain>
2022-03-25 23:05:19 +0530

    VITFED029

--- src/app.js
+++ src/app.js
@@ -2,85 +2,80 @@ const PLAYERS = [
     "Spiderman",
     "Captain America",
     "Wonderwoman",
-    // "Popcorn",
-    // "Gemwoman",
-    // "Bolt",
-    // "Antwoman",
-    // "Mask",
-    // "Tiger",
-    // "Captain",
-    // "Catwoman",
-    // "Fish",
-    // "Hulk",
-    // "Ninja",
-    // "Black Cat",
-    // "Volverine",
-    // "Thor",
-    // "Slayer",
-    // "Vader",
-    // "Slingo"
+    //"Popcorn",
+    //"Gemwoman",
+    //"Bolt",
+    //"Antwoman",
+    //"Mask",
+    //"Tiger",
+    //"Captain",
+    //"Catwoman",
+    //"Fish",
+    //"Hulk",
+    //"Ninja",
+    //"Black Cat",
+    //"Volverine",
+    //"Thor",
+    //"Slayer",
+    //"Vader",
+    //"Slingo"
 ];
 
-// Player Class
-class Player {
-    constructor(id, name, type) {
-        // Create member variables and assign values
-        // Type your code
+// initialize players with image and strength
+const initPlayers = (players) => {
+    let detailedPlayers = [];
 
-    }
+    // Instead of forloop use Map method
+    // Code here
+    detailedPlayers=players.map((item,index)=>{
+        let object={}
+        object.name=item;
+        object.strength=getRandomStrength();
+        object.image="images/super-"+(index+1)+".png"
+        object.type=randomType();
+        return object
+    })
 
-    // getting random strength
-    getRandomStrength = () => {
-        return Math.ceil(Math.random() * 100);
-    }
+    return detailedPlayers;
+} 
 
-    // Create a player for displaying
-    view = () => {
-        // Accumulate HTML template
-        // Type your code here
 
-    
-        return player;
-    }
+
+//generate random type
+const randomType=()=>{
+    let random=Math.round(Math.random())
+    return random===0?"hero":"villain"
 }
 
-// Superwar Class
-class Superwar {
-    constructor(players) {
-    // Create a field players 
-    // Use Map method to loop through players argument and create new players
+// getting random strength
+const getRandomStrength = () => {
+    return Math.ceil(Math.random() * 100);
+}
+
+// Build player template
+const buildPlayers = (players, type) => {
+    let fragment = '';
+
+    // Instead of using for loop
+    // Use chaining of Array methods - filter, map and join
     // Type your code here
+    let arr=players.map(item=>{
+        fragment+=`<div class="player">
+        <img src="${item.image}" alt="">
+        <div class="name">${item.name}</div>
+        <div class="strength">${item.strength}</div>
+        </div>`
+    })
+    return fragment;
+}
 
-    }
-
-    // Display players in HTML
-    viewPlayers = () => {
-        let team = document.getElementById('heroes');
-        team.innerHTML = '';
-        let fragment =
-            this.buildPlayers('hero');
-        team.append(fragment);
-
-        team = document.getElementById('villains');
-        team.innerHTML = '';
-        fragment =
-            this.buildPlayers('villain');
-        team.append(fragment);
-    }
-
-    // Build players fragment 
-    buildPlayers = (type) => {
-        let fragment = document.createDocumentFragment();
-        this.players
-            .filter(player => player.type == type)
-            .forEach(player => fragment.append(player.view()));
-        return fragment;
-    }
-
+// Display players in HTML
+const viewPlayers = (players) => {
+    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
 
 
 window.onload = () => {
-    const superwar = new Superwar(PLAYERS);
-    superwar.viewPlayers();
+    viewPlayers(initPlayers(PLAYERS));
 }
